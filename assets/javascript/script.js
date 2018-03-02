ae3a375db4c0d94c7ed117fd8b5c4220754513a8
unknown <you@example.com>
2018-03-01 20:20:42 -0700

    crystal change

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,50 @@
+(function(bounceit){
+
+  bounceit.fn.bounce = function(settings) {
+      if(typeof settings.interval == 'undefined'){
+          settings.interval = 100;
+      }
+
+      if(typeof settings.distance == 'undefined'){
+          settings.distance = 10;
+      }
+
+      if(typeof settings.times == 'undefined'){
+          settings.times = 4;
+      }
+
+      if(typeof settings.complete == 'undefined'){
+          settings.complete = function(){};
+      }
+
+      $(this).css('position','relative');
+
+      for(var iter=0; iter<(settings.times+1); iter++){
+          $(this).animate({ top:((iter%2 == 0 ? settings.distance : settings.distance * -1)) }, settings.interval);
+      }
+
+      $(this).animate({ top: 0}, settings.interval, settings.complete);  
+  };
+})(jQuery);
+
+
 $(document).ready(function () {
     var winsCounter = 0;
     var losesCounter = 0;
     var totalScore = 0;
     var random;
     var numberOptions;
+    var audio = new Audio("http://www.pacdv.com/sounds/mechanical_sound_effects/cling_1.wav");
+    
 
-   
+    $('.crystal-image').click(function(e){
+      audio.play();
+      $(this).bounce({
+          interval: 100,
+          distance: 20,
+          times: 5
+      });
+  });
 
 
 
@@ -65,6 +104,7 @@ $(document).ready(function () {
         $("#winScore").html(winsCounter);
         setgame();
         sevalues()
+
         
       }
 
@@ -73,7 +113,7 @@ $(document).ready(function () {
         $("#loseScore").html(losesCounter);
         setgame();
         sevalues()
-      
+
       }
 
     });
